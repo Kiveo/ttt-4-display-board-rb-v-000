b939af9c0f417d1a9752324210b1d233247f6490
Maverick Tesini <mavericktesini@gmail.com>
2018-02-12 16:47:10 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,6 +2,8 @@
 # out the current state.
 def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]) #board will be an array board[8] of 9 possible spaces
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
