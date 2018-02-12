88b7d73ea1c031983e6d234ab855397153282768
Maverick Tesini <mavericktesini@gmail.com>
2018-02-12 16:42:32 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board) #board will be an array board[8] of 9 possible spaces
+  puts " x | o | x "
+  puts " o | x | o "
+  puts " x | o | x "
+end
